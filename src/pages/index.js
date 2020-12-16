1cbf5535d75c290ec4f10823af78de059f95c4d2
Hemant Bhoi <56757949+hkbcode@users.noreply.github.com>
2020-12-16 13:36:51 +0530

    :zap: initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Button } from 'antd'
+
+
+const IndexPage = () =>{
+	return(
+		<div style={{textAlign:"center"}}>
+		<h1>Start Building websites with Antdesign</h1>
+		<Button type="primary">Get Started</Button>
+		</div>
+
+		)
+}
+
+
+export default IndexPage
